07f8fe9d0da0e05c8d6b47b4ee6360b9de3a9388
Mincong Wen <61830237+LockyWen@users.noreply.github.com>
2020-09-09 13:26:01 -0400

    HomePageController update

--- onlineShop/src/main/java/onlineShop/ApplicationConfig.java
+++ onlineShop/src/main/java/onlineShop/ApplicationConfig.java
@@ -1,5 +1,11 @@
 package onlineShop;
 
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 public class ApplicationConfig {
 
 }
